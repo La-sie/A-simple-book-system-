aa254d0c27c4cc252ca0b4befdf77e54905326cd
Sellasie <fsdjogatse@st.ug.edu.gh>
2022-07-29 13:10:29 +0000

    Update Book system.cpp

--- Book system.cpp
+++ Book system.cpp
@@ -3,7 +3,24 @@
 using namespace std;
 
 int main () {
+     string newBook;
+     string author;
+     string publisher;
+     string search;
 
+     cout << "***Welcome to Reina Books***" <<endl;
+
+     cout << "1.Add a new book" << endl;
+     cin >> newBook;
+
+     cout << "2.Search for a book" << endl;
+     cin >> search;
+
+     cout << "3. Enter the author's name " << endl;
+     cin >> author;
+
+     cout << "4. Enter the publisher's name " << endl;
+     cin >> publisher,
 
 return 0;
 }
